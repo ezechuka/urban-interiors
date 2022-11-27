8a7ae9ac15df31f53aee1b543f7cbe9e412cad7b
ezechuka <ezechukwuka9@gmail.com>
2022-11-27 16:27:33 +0100

    chore: Fetch key from .env file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    apiKey: process.env.NEXT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_APP_FIREBASE_AUTHDOMAIN,
+    projectID: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderID: process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appID: process.env.NEXT_APP_FIREBASE_APP_ID,
+    measurementID: process.env.NEXT_APP_FIREBASE_MEASUREMENT_ID,
+  }
 }
 
 module.exports = nextConfig
