413f6ac9ae2898c45c36be3509dbb8ab58e3bfa2
ezechuka <ezechukwuka9@gmail.com>
2022-11-27 16:29:29 +0100

    chore: Create firebase config variables

---
+++ firebase.js
@@ -0,0 +1,17 @@
+export const firebaseConfig = {
+
+    apiKey: process.env.apiKey,
+
+    authDomain: process.env.authDomain,
+
+    projectId: process.env.projectID,
+
+    storageBucket: process.env.storageBucket,
+
+    messagingSenderId: process.env.messagingSenderID,
+
+    appId: process.env.appID,
+
+    measurementId: process.env.measurementID
+
+}
