3a957cc1e42d5253a741ff1e9cdfe554df62251c
ezechuka <ezechukwuka9@gmail.com>
2022-12-03 13:36:59 +0100

    feat: Add product reducer to redux store

--- store/store.js
+++ store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { firebaseReducer } from 'react-redux-firebase'
+import { firebaseReducer, getFirebase } from 'react-redux-firebase'
 import { firestoreReducer } from 'redux-firestore'
 import thunk from 'redux-thunk'
 import storage from 'redux-persist/lib/storage'
 import persistReducer from 'redux-persist/lib/persistReducer';
 import persistStore from 'redux-persist/lib/persistStore';
+
+import productReducer from './productReducer';
 
 const persistFirebaseAuthConfig = {
     key: 'user',
@@ -16,10 +18,11 @@ const persistFirebaseReducer = persistReducer(persistFirebaseAuthConfig, firebas
 
 export const store = configureStore({
     reducer: {
-        persistFirebaseReducer,
-        firestoreReducer
+        persistFirebase: persistFirebaseReducer,
+        firestore: firestoreReducer,
+        product: productReducer
     },
-    middleware: [thunk]
+    middleware: [thunk.withExtraArgument(getFirebase)]
 })
 
 export const persistor = persistStore(store)
