ed4b4d96284a22e6489fd72330bd625b167c2c35
ezechuka <ezechukwuka9@gmail.com>
2022-12-20 06:51:52 +0100

    feat: Reducer to fetch a single product using the id

--- store/productReducer.js
+++ store/productReducer.js
@@ -1,15 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { doc, getDoc } from "firebase/firestore";
+
+const dataState = {
+    isLoading: true,
+    isFetching: false,
+    isLoaded: false,
+    error: null,
+    data: {}
+}
 
 export const productSlice = createSlice({
-    name: 'products',
-    initialState: [],
+    name: 'product',
+    initialState: dataState,
     reducers: {
-        fetchProducts(state, action) {
+        fetchProduct(state, action) {
             return action.payload
         }
     }
 })
 
-export const { fetchProducts } = productSlice.actions
+export const { fetchProduct } = productSlice.actions
 
 export default productSlice.reducer
+
+export const getProduct = (productId) => {
+    return async (dispatch, getState, { getFirebase }) => {
+        dispatch(fetchProduct({
+            ...dataState, isFetching: true
+        }))
+        const firestore = getFirebase().firestore()
+        const productDocRef = doc(firestore, 'products', `${productId}`)
+        try {
+            const product = await getDoc(productDocRef)
+            if (product.exists)
+                dispatch(fetchProduct({
+                    ...dataState,
+                    isLoading: false,
+                    isFetching: false,
+                    isLoaded: true,
+                    data: product.data()
+                }))
+        } catch (e) {
+            dispatch(fetchProduct({
+                ...dataState.data,
+                isLoading: false,
+                isFetching: false,
+                isLoaded: true,
+                error: e,
+                data: null
+            }))
+        }
+    }
+}
