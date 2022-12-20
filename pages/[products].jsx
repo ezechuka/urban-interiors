284e32530c8275be0e847754de40549fc32d3ace
ezechuka <ezechukwuka9@gmail.com>
2022-12-20 06:56:10 +0100

    feat: Implement products listing page

---
+++ pages/[products].jsx
@@ -0,0 +1,294 @@
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Breadcrumb, BreadcrumbItem, Button, Checkbox, Divider, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, Grid, HStack, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, RangeSlider, RangeSliderFilledTrack, RangeSliderThumb, RangeSliderTrack, Skeleton, SkeletonCircle, Stack, Text, useDisclosure, VStack } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { CaretRight, FunnelSimple } from 'phosphor-react'
+import { useEffect, useRef, useState } from 'react'
+import { connect, useDispatch, useSelector } from 'react-redux'
+import Link from 'next/link'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
+
+import ProductItem from '../components/product/ProductItem'
+import { getProducts } from '../store/productsReducer'
+
+const LoadingSkeleton = () => {
+    return (
+        <Grid
+            gridTemplateColumns={'repeat(5, 1fr)'}
+            gap={8}
+            w={'full'}
+            marginY={8}>
+            {
+                [...Array(10).keys()].map(item => (
+                    <VStack
+                        key={item}
+                        justifyContent={'start'}
+                        alignItems={'start'}>
+                        <Skeleton
+                            width={'220px'}
+                            height={'100px'}
+                            rounded={'lg'}
+                            fadeDuration={2}
+                        />
+                        <Skeleton
+                            width={'140px'}
+                            height={'10px'}
+                            rounded={'lg'}
+                            fadeDuration={2}
+                        />
+                        <Skeleton
+                            width={'70px'}
+                            height={'10px'}
+                            rounded={'lg'}
+                            fadeDuration={2}
+                        />
+                    </VStack>
+                ))
+            }
+        </Grid>
+    )
+}
+
+const FilterAccordionItem = ({ accordionTitle, items }) => {
+    return (
+        <AccordionItem>
+            <h2>
+                <AccordionButton>
+                    <Box flex='1' textAlign='left' textColor={'black'} fontWeight={'medium'}>
+                        {accordionTitle}
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+                <VStack
+                    justifyContent={'start'}
+                    alignItems={'start'}>
+                    {
+                        items.map(item => (
+                            <Checkbox colorScheme={'orange'}>
+                                {item}
+                            </Checkbox>
+                        ))
+                    }
+                </VStack>
+            </AccordionPanel>
+        </AccordionItem>
+    )
+}
+
+const FilterDrawer = ({ isOpen, onClose, btnRef }) => {
+
+    const [priceRange, setPriceRange] = useState([100000, 250000])
+
+    return (
+        <>
+            <Drawer
+                isOpen={isOpen}
+                placement={'left'}
+                onClose={onClose}
+                finalFocusRef={btnRef}>
+
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader mt={6}>
+                        <HStack spacing={3}
+                            alignItems={'center'}>
+                            <FunnelSimple size={24} />
+                            <Text>Filter items by</Text>
+                        </HStack>
+                    </DrawerHeader>
+
+                    <DrawerBody>
+                        <Accordion allowMultiple>
+                            <FilterAccordionItem
+                                accordionTitle={'Categories'}
+                                items={['Couch', 'Bed pallet', 'Shoe rack',
+                                    'Ward robe', 'Shelf', 'TV Console', 'Table & Chair']}
+                            />
+
+                            <FilterAccordionItem
+                                accordionTitle={'Colors'}
+                                items={['White', 'Black', 'Velvet', 'Matte', 'Brown']}
+                            />
+
+                            <Text
+                                textColor={'black'}
+                                fontWeight={'semibold'}
+                                marginTop={4}>
+                                Price (Naira)
+                            </Text>
+
+                            <RangeSlider aria-label={['min', 'max']}
+                                defaultValue={[100000, 250000]}
+                                marginTop={2}
+                                onChange={(val) => setPriceRange(val)}
+                                min={10000}
+                                max={500000}>
+                                <RangeSliderTrack>
+                                    <RangeSliderFilledTrack bgColor={'gold.500'} />
+                                </RangeSliderTrack>
+
+                                <RangeSliderThumb boxSize={5} index={0} bgColor={'gray.100'} />
+                                <RangeSliderThumb boxSize={5} index={1} bgColor={'gray.100'} />
+                            </RangeSlider>
+                            <Flex
+                                justifyContent={'space-between'}>
+                                <Text
+                                    textColor={'gray.900'}
+                                    fontSize={'xs'}
+                                    fontWeight={'medium'}>
+                                    {`₦${new Intl.NumberFormat().format(priceRange[0])}`}
+                                </Text>
+
+                                <Text
+                                    textColor={'gray.900'}
+                                    fontSize={'xs'}
+                                    fontWeight={'medium'}>
+                                    {`₦${new Intl.NumberFormat().format(priceRange[1])}`}
+                                </Text>
+                            </Flex>
+
+                            <Button
+                                variant={'ghost'}
+                                borderWidth={1}
+                                marginTop={4}
+                                paddingX={18}
+                                paddingY={2}
+                                _hover={{ bgColor: 'blackAlpha.100', color: 'gold.500' }}>
+                                Apply
+                            </Button>
+                        </Accordion>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        </>
+    )
+}
+
+const Products = ({ getProducts }) => {
+    const router = useRouter()
+    const path = router.asPath.replace('/', '')
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const buttonDrawerRef = useRef()
+
+    useEffect(() => {
+        getProducts(path)
+    }, [path])
+
+    const { isLoading, isFetching, isLoaded, error, data }
+        = useSelector((state) => state.products)
+
+    // if (isLoading) return <LoadingSkeleton />
+
+    if (error) return <Text>An error occurred.</Text>
+
+    return (
+        <Flex
+            as={'section'}
+            paddingX={12}
+            paddingY={8}
+            flexDirection={'column'}
+            justifyContent={'center'}
+            alignItems={'center'}>
+
+            <ToastContainer />
+
+            <Flex
+                alignItems={'center'}
+                w={'full'}>
+                <IconButton
+                    variant={'ghost'}
+                    ref={buttonDrawerRef}
+                    onClick={onOpen}
+                    marginEnd={8}
+                    icon={<FunnelSimple size={24} weight={'regular'} />}
+                    _hover={{ background: 'gray.100', color: 'gold.500' }} />
+
+                <Breadcrumb
+                    spacing={2}
+                    separator={<CaretRight color={'#3e3e3e'} weight={'bold'} size={14} />}
+                    fontWeight={'medium'}
+                    fontSize={'sm'}
+                    textDecoration={'none'}>
+                    <BreadcrumbItem textColor={'gray.600'} _hover={{ color: 'gold.500' }}>
+                        <Link href='/'>Home</Link>
+                    </BreadcrumbItem>
+
+                    <BreadcrumbItem textColor={'gray.900'} textTransform={'capitalize'} isCurrentPage>
+                        <Text>{path.replace('-', ' ')}</Text>
+                    </BreadcrumbItem>
+                </Breadcrumb>
+
+            </Flex>
+
+            {
+                isFetching ?
+                    <LoadingSkeleton />
+                    :
+                    <Grid
+                        gridTemplateColumns={'repeat(5, 1fr)'}
+                        gap={8}
+                        w={'full'}
+                        marginY={8}>
+                        {
+                            Object.values(data).map(product => (
+                                <ProductItem
+                                    key={product.pid}
+                                    productId={product.pid}
+                                    productTitle={product.title}
+                                    productImg={product.img[0]}
+                                    productPrice={product.price}
+                                />))
+                        }
+                    </Grid>
+            }
+            {/* <Flex
+                flexDirection={'column'}
+                alignItems={'start'}
+                width={'full'}
+                marginTop={8}>
+
+                <Text
+                    fontSize={'xl'}
+                    textColor={'black'}>
+                    Recently Viewed
+                </Text>
+
+                <Grid
+                    gridTemplateColumns={'repeat(5, 1fr)'}
+                    gap={8}
+                    w={'full'}
+                    marginY={8}>
+
+                    {
+                        [1, 2, 3, 4, 5].map(i => (
+                            <ProductItem
+                                featTitle={'Three seater sofa with an elongated end'}
+                                featImg={sofa}
+                                featPrice={'₦3,000'}
+                            />))
+                    }
+                </Grid> 
+
+                </Flex> */}
+
+            <FilterDrawer
+                isOpen={isOpen}
+                onClose={onClose}
+                btnRef={buttonDrawerRef}
+            />
+
+        </Flex>
+    )
+}
+
+export const matchDispatchToProps = dispatch => {
+    return {
+        getProducts: (path) =>
+            dispatch(getProducts(path))
+    }
+}
+
+export default connect(null, matchDispatchToProps)(Products)
