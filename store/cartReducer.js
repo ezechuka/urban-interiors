6d4aa867c69bcb8ccf3655a8bf1a407a5e6a6bb5
ezechuka <ezechukwuka9@gmail.com>
2022-12-20 06:53:16 +0100

    feat: Implement reducer for cart related functionalities

---
+++ store/cartReducer.js
@@ -0,0 +1,143 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        info: '',
+        items: []
+    },
+    reducers: {
+        addItemToCart(state, action) {
+            if (action.payload.info === 'ADD')
+                toast.success('Item has been added to Cart')
+            else if (action.payload.info === 'FAIL')
+                toast.info('Item already exists in Cart')
+            return { info: action.payload.info }
+        },
+        increaseItemQuantity(state, action) {
+            return state
+        },
+        decreaseItemQuantity(state, action) {
+            return state
+        },
+        deleteItemFromCart(state, action) {
+            if (action.payload.info === 'DELETE')
+                toast.info('Item has been deleted from Cart')
+            return { info: action.payload.info }
+        }
+    }
+})
+
+export const
+    { addItemToCart, increaseItemQuantity, decreaseItemQuantity, deleteItemFromCart } 
+    = cartSlice.actions
+
+export default cartSlice.reducer
+
+export const addToCart = (productId, cartItem, cart) => {
+    return async (dispatch, getState, { getFirebase }) => {
+        if (Object.keys(cart).includes(productId)) {
+            dispatch(addItemToCart(
+                {
+                    info: 'FAIL'
+                }
+            ))
+            return
+        }
+        const firestore = getFirebase().firestore()
+        const userId = getState().persistFirebase.auth.uid
+        firestore
+            .collection('users')
+            .doc(userId)
+            .update({
+                'cart': {
+                    totalItems: Number(cart.totalItems) + 1,
+                    totalPrice: Number(cart.totalPrice) + Number(cartItem.price),
+                    items: {
+                        ...cart.items,
+                        [`${productId}`]: {
+                            quantity: 1
+                        }
+                    }
+                }
+            }).then(() => {
+                dispatch(addItemToCart({
+                    info: 'ADD'
+                }))
+            })
+    }
+}
+
+export const increaseQuantity = (productId, cartItem, cart) => {
+    let cartProduct = cart.items[productId]
+    console.log(cartProduct)
+    let newCartItem = {
+        quantity: cartProduct.quantity + 1
+    }
+
+    cart.items = { ...cart.items, [productId]: newCartItem }
+    return async (dispatch, getState, { getFirebase }) => {
+        const firestore = getFirebase().firestore()
+        const userId = getState().persistFirebase.auth.uid
+        firestore
+            .collection('users')
+            .doc(userId)
+            .update({
+                'cart': {
+                    totalItems: Number(cart.totalItems) + 1,
+                    totalPrice: Number(cart.totalPrice) + Number(cartItem.price),
+                    items: cart.items
+                }
+            }).then(() => {
+                dispatch(increaseItemQuantity())
+            })
+    }
+}
+
+export const decreaseQuantity = (productId, cartItem, cart) => {
+    let cartProduct = cart.items[productId]
+    let newCartItem = {
+        quantity: cartProduct.quantity - 1
+    }
+
+    cart.items = { ...cart.items, [productId]: newCartItem }
+    return async (dispatch, getState, { getFirebase }) => {
+        const firestore = getFirebase().firestore()
+        const userId = getState().persistFirebase.auth.uid
+        firestore
+            .collection('users')
+            .doc(userId)
+            .update({
+                'cart': {
+                    totalItems: Number(cart.totalItems) - 1,
+                    totalPrice: Number(cart.totalPrice) - Number(cartItem.price),
+                    items: cart.items
+                }
+            }).then(() => {
+                dispatch(decreaseItemQuantity())
+            })
+    }
+}
+
+export const deleteFromCart = (productId, cartItem, quantity, cart) => {
+    return async (dispatch, getState, { getFirebase }) => {
+        const firestore = getFirebase().firestore()
+        const userId = getState().persistFirebase.auth.uid
+        delete cart.items[productId]
+        firestore
+            .collection('users')
+            .doc(userId)
+            .update({
+                'cart': {
+                    totalItems: Number(cart.totalItems) - Number(quantity),
+                    totalPrice: Number(cart.totalPrice) - (Number(cartItem.price) * Number(quantity)),
+                    items: cart.items
+                }
+            }).then(() => {
+                dispatch(deleteItemFromCart({
+                    info: 'DELETE'
+                }))
+            })
+    }
+}
