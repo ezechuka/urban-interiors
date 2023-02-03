1d081e9e82ab0749b58798e1d244020aa0f8b52f
ezechuka <ezechuka9@gmail.com>
2023-02-03 15:55:12 +0100

    fix: Adjust padding value for featured section

--- components/featured/Featured.jsx
+++ components/featured/Featured.jsx
@@ -66,7 +66,7 @@ const FeaturedItem = ({ productId, productName, productPrice,
             flexDirection={'column'}
             justifyContent={'center'}
             transition={'all'}
-            p={2}
+            p={{base: 1, md: 2}}
             bgColor={'blackAlpha.200'}
             position={'relative'}
             _hover={{ shadow: 'md' }}>
@@ -186,8 +186,8 @@ const ProductCollection = ({ productState }) => {
                 md: 'repeat(3, 1fr)',
                 lg: 'repeat(5, 1fr)'
             }}
-            rowGap={{ base: 3, lg: 8 }}
-            columnGap={{ base: 3, lg: 8 }}
+            rowGap={{ base: 2, lg: 8 }}
+            columnGap={{ base: 2, lg: 8 }}
             paddingX={{ base: 1, lg: 0 }}
             marginTop={8}>
 
