0027bdc51edef1589fc15d85b1e1549a73b51149
ezechuka <ezechuka9@gmail.com>
2023-02-03 10:41:04 +0100

    feat: Add responsive styles for footer section

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { Divider, Flex, Grid, HStack, Icon, IconButton, Stack, Text } from '@chakra-ui/react';
+import { Divider, Flex, Grid, HStack, Icon, IconButton, Stack, Text, VStack } from '@chakra-ui/react';
 import Link from 'next/link'
 import { At, InstagramLogo, MapPinLine, TwitterLogo, WhatsappLogo } from 'phosphor-react';
 import { useSelector } from 'react-redux';
@@ -13,18 +13,19 @@ const Footer = () => {
             flexDir={'column'}>
 
             <Flex
+                flexDirection={{ base: 'column', lg: 'row' }}
                 justifyContent={'space-between'}
-                alignItems={'start'}
+                alignItems={{ base: 'center', lg: 'start' }}
                 w={'full'}
                 px={12}>
                 <Flex
                     flexDirection={'column'}
                     justifyContent={'space-between'}
-                    alignItems={'start'}
+                    alignItems={{base: 'center', lg: 'start'}}
                     flexGrow={1}>
                     <Text
                         fontWeight={'black'}
-                        fontSize={'3xl'}
+                        fontSize={{base: 'xl', lg: '3xl'}}
                         textColor={'white'}
                         textTransform={'capitalize'}>
                         fobath woodwork
@@ -35,52 +36,17 @@ const Footer = () => {
                         fontSize={'sm'}
                         textColor={'white'}
                         maxWidth={'md'}
-                        mt={5}>
+                        mt={5}
+                        textAlign={{base: 'center', lg: 'start'}}>
                         Fobath Woodwork is a company that specializes in creating high-quality, handcrafted household furniture.
                         We also offer assembly services for IKEA products, ensuring that customers can enjoy their new furniture quickly and easily.
                     </Text>
 
-                    <Stack
-                        direction={'row'}
-                        spacing={4}
-                        mt={5}>
-
-                        <Link
-                            href={'https://twitter.com/Fobath_woodwork'} target={'_blank'}>
-                            <IconButton
-                                aria-label={'twitter social button'}
-                                color={'white'}
-                                bgColor={'whiteAlpha.200'}
-                                variant={'ghost'}
-                                icon={
-                                    <TwitterLogo size={24} weight={'fill'} alt={'Twitter'} />
-                                }
-                                _hover={{
-                                    color: 'gold.500'
-                                }}
-                            />
-                        </Link>
-                        <Link href={'https://www.instagram.com/fobath_woodwork/'} target={'_blank'}>
-                            <IconButton
-                                aria-label={'instagram social button'}
-                                color={'white'}
-                                bgColor={'whiteAlpha.200'}
-                                variant={'ghost'}
-                                icon={
-                                    <InstagramLogo size={24} weight={'fill'} alt={'Instagram'} />
-                                }
-                                _hover={{
-                                    color: 'gold.500'
-                                }}
-                            />
-
-                        </Link>
-                    </Stack>
                 </Flex>
 
-                <Grid
-                    gridTemplateColumns={'repeat(1, 1fr)'}
-                    rowGap={4}>
+                <VStack
+                    alignItems={{ base: 'center', lg: 'start' }}
+                    mt={{base: 8, lg: 0}}>
                     <Text
                         color={'white'}
                         fontWeight={'semibold'}
@@ -131,12 +97,12 @@ const Footer = () => {
                             Terms and Conditions
                         </Text>
                     </Link>
-                </Grid>
+                </VStack>
 
-                <Grid
-                    gridTemplateColumns={'repeat(1, 1fr)'}
-                    rowGap={3}
-                    ml={20}>
+                <VStack
+                    alignItems={{ base: 'center', lg: 'start' }}
+                    ml={{base: 0, lg: 20}}
+                    mt={{base: 8, lg: 0}}>
                     <Text
                         color={'white'}
                         fontWeight={'semibold'}
@@ -218,12 +184,12 @@ const Footer = () => {
                             Account
                         </Text>
                     </Link>
-                </Grid>
+                </VStack>
 
-                <Grid
-                    gridTemplateColumns={'repeat(1, 1fr)'}
-                    rowGap={3}
-                    ml={20}>
+                <VStack
+                    alignItems={{base: 'center', lg: 'start'}}
+                    ml={{base: 0, lg: 20}}
+                    mt={{base: 8, lg: 0}}>
                     <Text
                         color={'white'}
                         fontWeight={'semibold'}
@@ -273,7 +239,43 @@ const Footer = () => {
                             fobath@mail.com
                         </Text>
                     </HStack>
-                </Grid>
+
+                    <Stack
+                        direction={'row'}
+                        spacing={4}>
+
+                        <Link
+                            href={'https://twitter.com/Fobath_woodwork'} target={'_blank'}>
+                            <IconButton
+                                aria-label={'twitter social button'}
+                                color={'white'}
+                                bgColor={'whiteAlpha.200'}
+                                variant={'ghost'}
+                                icon={
+                                    <TwitterLogo size={24} weight={'fill'} alt={'Twitter'} />
+                                }
+                                _hover={{
+                                    color: 'gold.500'
+                                }}
+                            />
+                        </Link>
+                        <Link href={'https://www.instagram.com/fobath_woodwork/'} target={'_blank'}>
+                            <IconButton
+                                aria-label={'instagram social button'}
+                                color={'white'}
+                                bgColor={'whiteAlpha.200'}
+                                variant={'ghost'}
+                                icon={
+                                    <InstagramLogo size={24} weight={'fill'} alt={'Instagram'} />
+                                }
+                                _hover={{
+                                    color: 'gold.500'
+                                }}
+                            />
+
+                        </Link>
+                    </Stack>
+                </VStack>
             </Flex>
 
             <Divider mt={8} mb={4} orientation={'horizontal'} bgColor={'whiteAlpha.500'} />
