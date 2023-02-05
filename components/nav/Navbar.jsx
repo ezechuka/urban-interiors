d87877006490b2937901d175c9a7fc0da56ce089
ezechuka <ezechuka9@gmail.com>
2023-02-05 23:01:20 +0100

    feat: Implement mobile menu drawer

--- components/nav/Navbar.jsx
+++ components/nav/Navbar.jsx
@@ -1,5 +1,6 @@
-import { Avatar, Box, Button, Circle, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, HStack, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList, Text, Tooltip, useDisclosure } from '@chakra-ui/react'
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Avatar, Box, Button, Circle, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, HStack, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, Link, Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList, Text, Tooltip, useDisclosure, VStack } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
+import { Link as NextLink } from 'next/link'
 import { AngularLogo, Heart, List, MagnifyingGlass, Package, ShoppingCart, SignOut, SquaresFour, User, UserCircle } from 'phosphor-react'
 import { useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -12,6 +13,7 @@ const Navbar = () => {
     const dispatch = useDispatch()
     const { isOpen, onOpen, onClose } = useDisclosure()
     const btnRef = useRef()
+    const menuRef = useRef()
 
     return (
         <Flex
@@ -53,7 +55,7 @@ const Navbar = () => {
 
             <Text
                 fontWeight={'semibold'}
-                fontSize={{base: 'sm', md: 'md'}}
+                fontSize={{ base: 'sm', md: 'md' }}
                 letterSpacing={'widest'}
                 textColor={'black'}
                 textTransform={'uppercase'}
@@ -265,25 +267,232 @@ const Navbar = () => {
                 </Box>
             </HStack>
 
+            <Drawer
+                isOpen={isOpen}
+                placement={'right'}
+                onClose={onClose}
+                finalFocusRef={menuRef}>
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Fobath Woodwork</DrawerHeader>
+
+                    <DrawerBody>
+                        {
+                            auth.isEmpty ?
+                                <VStack
+                                    w={'full'}
+                                    h={'full'}
+                                    justifyContent={'center'}>
+                                    <Text
+                                        fontSize={'3xl'}
+                                        fontWeight={'bold'}>
+                                        Get started
+                                    </Text>
+                                    <Button
+                                        variant={'solid'}
+                                        w={'100%'}
+                                        mt={8}
+                                        fontWeight={'medium'}
+                                        shadow={'lg'}
+                                        transition={'all .5s'}
+                                        textTransform={'uppercase'}
+                                        _hover={{
+                                            boxShadow: 'lg',
+                                            backgroundColor: 'gold.500',
+                                            borderColor: 'gold.500',
+                                            color: 'white',
+                                            transform: 'scale(1.05)'
+                                        }}
+                                        onClick={() => router.push('/signup')}>
+                                        Signup
+                                    </Button>
+                                    <Button
+                                        variant={'ghost'}
+                                        borderWidth={1}
+                                        borderColor={'gray.300'}
+                                        w={'100%'}
+                                        transition={'all .5s'}
+                                        fontWeight={'medium'}
+                                        onClick={() => router.push('/login')}>
+                                        login
+                                    </Button>
+                                </VStack>
+                                :
+                                <VStack>
+                                    <Flex
+                                        flexDirection={'column'}
+                                        alignItems={'center'}
+                                        mb={4}>
+                                        <Avatar name={auth.displayName} src={auth.avatarUrl} />
+                                        <Text
+                                            fontWeight={'medium'}>
+                                            {auth.displayName}
+                                        </Text>
+                                        <Text
+                                            fontWeight={'normal'}
+                                            fontSize={'sm'}>
+                                            {auth.email}
+                                        </Text>
+                                    </Flex>
+                                    <Accordion allowToggle w={'full'}>
+                                        <AccordionItem fontSize={'sm'}>
+                                            <h2>
+                                                <AccordionButton>
+                                                    <Text fontWeight={'medium'} flex='1' textAlign='left'>
+                                                        Category
+                                                    </Text>
+                                                    <AccordionIcon />
+                                                </AccordionButton>
+                                            </h2>
+                                            <AccordionPanel>
+                                                <VStack alignItems={'start'}>
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/sofa'}>
+                                                        Sofa
+                                                    </Link>
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/bed'}>
+                                                        Bed
+                                                    </Link>
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/shoe-rack'}>
+                                                        Shoe Rack
+                                                    </Link>
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/ward-robe'}>
+                                                        Ward Robe
+                                                    </Link>
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/shelf'}>
+                                                        Shelf
+                                                    </Link>
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/tv-console'}>
+                                                        TV Console
+                                                    </Link>
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/table-and-chair'}>
+                                                        Table and chair
+                                                    </Link>
+                                                </VStack>
+                                            </AccordionPanel>
+                                        </AccordionItem>
+                                    </Accordion>
+
+                                    <Accordion defaultIndex={[0]} allowToggle w={'full'}>
+                                        <AccordionItem fontSize={'sm'}>
+                                            <h2>
+                                                <AccordionButton>
+                                                    <Text fontWeight={'medium'} flex='1' textAlign='left'>
+                                                        Account
+                                                    </Text>
+                                                    <AccordionIcon />
+                                                </AccordionButton>
+                                            </h2>
+                                            <AccordionPanel pb={4}>
+                                                <VStack alignItems={'start'}>
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/wishlist'}>
+                                                        Wishlist
+                                                    </Link>
+
+                                                    <Link
+                                                        as={NextLink}
+                                                        href={'/orders'}>
+                                                        Orders
+                                                    </Link>
+                                                </VStack>
+
+                                                <Button
+                                                    variant={'solid'}
+                                                    w={'100%'}
+                                                    mt={8}
+                                                    fontWeight={'medium'}
+                                                    shadow={'lg'}
+                                                    py={5}
+                                                    transition={'all .5s'}
+                                                    textTransform={'uppercase'}
+                                                    onClick={() => router.push('/cart')}>
+                                                    Cart
+                                                </Button>
+
+                                                <Button
+                                                    variant={'ghost'}
+                                                    w={'100%'}
+                                                    mt={4}
+                                                    py={5}
+                                                    fontWeight={'medium'}
+                                                    shadow={'sm'}
+                                                    textColor={'red.400'}
+                                                    borderWidth={'1px'}
+                                                    borderColor={'gray.200'}
+                                                    transition={'all .5s'}
+                                                    textTransform={'uppercase'}
+                                                    onClick={() => {
+                                                        dispatch(signOut())
+                                                        router.replace('/')
+                                                    }}>
+                                                    logout
+                                                </Button>
+                                            </AccordionPanel>
+                                        </AccordionItem>
+                                    </Accordion>
+                                </VStack>
+                        }
+                    </DrawerBody>
+
+                </DrawerContent>
+            </Drawer>
+
             {/* Mobile hamburger icon button */}
-            <IconButton
-                aria-label={'mobile menu'}
-                color={'black'}
-                variant={'ghost'}
-                justifyContent={'end'}
-                display={{base: 'flex', lg: 'none'}}
-                icon={
-                    <Tooltip
-                        hasArrow
-                        label={'Cart'}
-                        placement={'bottom'}
-                        textColor={'white'}
-                        bgColor={'gray.900'}>
-                        <List size={24} weight={'regular'} alt={''} />
-                    </Tooltip>
+            <Box
+                position={'relative'}
+                display={{ base: 'flex', lg: 'none' }}>
+                <IconButton
+                    aria-label={'mobile menu'}
+                    color={'black'}
+                    variant={'ghost'}
+                    justifyContent={'end'}
+                    icon={
+                        <Tooltip
+                            hasArrow
+                            label={'Cart'}
+                            placement={'bottom'}
+                            textColor={'white'}
+                            bgColor={'gray.900'}>
+                            <List size={28} weight={'regular'} alt={''} />
+                        </Tooltip>
+                    }
+                    onClick={() => onOpen()} />
+
+                {
+                    cart && cart.totalItems > 0 &&
+                    <Circle
+                        size='20px'
+                        bg='gold.500'
+                        color='white'
+                        position={'absolute'}
+                        top={0}
+                        right={-1}>
+
+                        <Text
+                            fontSize={'xs'}
+                            fontWeight={'medium'}>
+                            {cart ? cart.totalItems : 0}
+                        </Text>
+                    </Circle>
                 }
-                onClick={() => {} } />
-        </Flex >
+            </Box>
+        </Flex>
     )
 }
 
